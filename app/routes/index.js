80cc5cfe2ed5bf3b0e1c3cf7df532db6ea74f5ac
Erin Sparling <erin@everyplace.net>
2014-11-09 18:31:42 -0500

    Added tag and location search.

--- app/routes/index.js
+++ app/routes/index.js
@@ -35,3 +35,27 @@ exports.instagram_search = function(req, res) {
 
 
 };
+
+exports.instagram_location = function(req, res) {
+  ig.use({ client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET});
+
+  ig.location_search({ lat: parseFloat(req.params.lat), lng: parseFloat(req.params.long) }, function(err, result, remaining, limit) {
+
+    var resultCache = [];
+    for(i in result) {
+      ig.location_media_recent(result[i].id, {min_timestamp:1388534400}, function(err, result, pagination, remaining, limit) {
+        // console.log(err, result);
+        for(j in result) {
+          resultCache.push(result[j]);
+        }
+
+        if(i = result.length -1 ){
+          res.end(JSON.stringify(resultCache));
+        }
+      });
+
+    }
+
+  });
+
+};
