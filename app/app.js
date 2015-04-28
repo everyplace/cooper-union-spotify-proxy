ac292b4d719c89079fafd55c72a7f4131b00dbc9
Erin Sparling <erin@everyplace.net>
2015-04-27 20:42:42 -0400

    Made changes to adapt the instagram api for Nasa\'s stars api

--- app/app.js
+++ app/app.js
@@ -26,16 +26,9 @@ app.configure('development', function(){
 
 //configure routes
 app.get('/', routes.index);
-// app.get('/weather/:state/:city', routes.weather);
-// app.get('/search/tag/:search', routes.json, routes.instagram_search);
-app.get('/search/location/:lat/:long', routes.json, routes.instagram_location);
-app.get('/search/user/:username', routes.json, routes.instagram_user_search);
-app.get('/search/tag/:tag', routes.json, routes.instagram_tag_media_recent);
-app.get('/user/:username', routes.json, routes.instagram_user_info);
-app.get('/search/venue/:id', routes.json, routes.instagram_location_venue);
-
+app.get('/api/v1/stars', routes.json, routes.stars);
 
 //initiate the app server
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("cooper-union-weather-proxy running on port " + app.get('port'));
+  console.log("cooper-union-stars-proxy running on port " + app.get('port'));
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,7 @@
-var request = require('request')
-  , ig = require('instagram-node').instagram();
-
+var request = require('request');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Cooper Union Instagram Proxy' });
+  res.render('index', { title: 'Cooper Union Stars Proxy' });
 };
 
 exports.json = function(req, res, next) {
@@ -15,86 +13,15 @@ exports.json = function(req, res, next) {
   next();
 };
 
-exports.instagram_search = function(req, res) {
 
-  ig.use({ client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET});
-  ig.tag_media_recent(req.params.search, function(err, medias, pagination, remaining, limit) {
+exports.stars = function(req, res, next) {
 
-    res.end(JSON.stringify(medias));
-  });
+  request('http://star-api.herokuapp.com/api/v1/stars', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
 
-
-};
-
-exports.instagram_location = function(req, res) {
-  ig.use({ client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET});
-
-  ig.location_search({ lat: parseFloat(req.params.lat), lng: parseFloat(req.params.long) }, function(err, result, remaining, limit) {
-
-    var resultCache = [];
-    for(i in result) {
-      ig.location_media_recent(result[i].id, {min_timestamp:1388534400}, function(err, result, pagination, remaining, limit) {
-        // console.log(err, result);
-        for(j in result) {
-          resultCache.push(result[j]);
-        }
-
-        if(i = result.length -1 ){
-          res.end(JSON.stringify(resultCache));
-        }
-      });
+      res.send(body);
 
     }
-
-  });
-
-};
-
-
-exports.instagram_user_search = function(req, res) {
-  ig.use({ client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET});
-  ig.user_search(req.params.username, function(err, users, remaining, limit) {
-
-    res.end(JSON.stringify(users));
-  });
-};
-
-exports.instagram_user_info = function(req, res) {
-  ig.use({ client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET});
-  ig.user(req.params.username, function(err, result, remaining, limit) {
-
-    res.end(JSON.stringify(result));
-  });
-};
-
-exports.instagram_tag_media_recent = function(req, res) {
-  ig.use({ client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET});
-  var count = (req.query.count && (req.query.count <= 100)) ? req.query.count : 15;
-  var max_tag_id = (req.query.max_tag_id) ? req.query.max_tag_id : null;
-  var min_tag_id = (req.query.min_tag_id) ? req.query.min_tag_id : null;
-  ig.tag_media_recent(req.params.tag, {count:count, min_tag_id: min_tag_id, max_tag_id: max_tag_id}, function(err, medias, pagination, remaining, limit) {
-    if(max_tag_id || min_tag_id) {
-      res.end(JSON.stringify({media:medias,pagination:pagination}));
-    } else {
-      res.end(JSON.stringify(medias));  
-    }
-
-  });
-};
-
-exports.instagram_location_venue = function(req, res) {
-
-  console.log("debugging")
-  ig.use({ client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET});
-
-  ig.location(req.params.id, function(err, result, remaining, limit) {
-    console.log(err);
-    res.end(JSON.stringify(result));
-  });
+  })
 
 };
-
-
-
-
-// ig.tag_media_recent('tag', [options,] function(err, medias, pagination, remaining, limit) {});
